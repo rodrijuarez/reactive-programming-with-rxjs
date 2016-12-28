476fb78b5f436a2dbb1d0e622b24ee848c0bf506
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2016-12-28 11:45:30 -0300

    Changed method to create Observable
    Trying out ‘create’ method instead of ‘from’, also we’re throwing an
    error just to test out the error callback of the observer

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import { Observable } from 'rxjs';
 
 let numbers = [1, 5, 10];
-let source = Observable.from(numbers);
+let source = Observable.create(observer => {
+	for (let n of numbers) {
+		if (n === 5) observer.error("Something went wrong");
+		observer.next(n);
+	}
+	observer.complete();
+});
 
 
 source.subscribe(
