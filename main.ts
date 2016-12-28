9b0fac2be38a12697aa6c5ada9d0662660ea9a77
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2016-12-28 12:38:43 -0300

    Feature
    When the mouse moves a red circle will follow the mouse movement

--- main.ts
+++ main.ts
@@ -1,30 +1,24 @@
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
+import { Observable } from 'rxjs';
 
-let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
-	let index = 0;
-	let produceValue = () => {
-		observer.next(numbers[index++]);
+let circle = document.getElementById("circle");
 
-		if (index < numbers.length) {
-			setTimeout(produceValue, 250);
-		} else {
-			observer.complete();
+let source = Observable.fromEvent(document, "mousemove")
+	.map((e: MouseEvent) => {
+		return {
+			x: e.clientX, y: e.clientY
 		}
-	}
+	})
+	.filter((value) => value.x < 500)
+	.delay(300);
 
-	produceValue();
-})
-.map(n => n * 2)
-.filter(n => n > 5);
 
+function onNext(value) {
+	circle.style.left = `${value.x}px`;
+	circle.style.top = `${value.y}px`;
+}
 
 source.subscribe(
-	(value) => {
-		console.log(`value: ${value}`);
-	},
+	onNext,
 	(e) => {
 		console.log(`error: ${e}`);
 	},
