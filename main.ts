13a9e767d3dfa386cb2e1dc6d2e7f5ffcad9a139
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2016-12-28 11:39:36 -0300

    Implementation of observer (Callbacks)

--- main.ts
+++ main.ts
@@ -3,18 +3,15 @@ import { Observable } from 'rxjs';
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
-class MyObserver {
-	next(value) {
+
+source.subscribe(
+	(value) => {
 		console.log(`value: ${value}`);
-	}
-
-	error(e) {
+	},
+	(e) => {
 		console.log(`error: ${e}`);
-	}
-
-	complete() {
+	},
+	() => {
 		console.log('complete');
 	}
-}
-
-source.subscribe(new MyObserver());
+);
