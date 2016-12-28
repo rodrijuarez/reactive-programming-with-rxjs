f6733000303626882cd9be2dc9b5978a56ec66d6
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2016-12-28 14:05:56 -0300

    Code Improvement
    Now we use flatMap creating a pipeline for processing the data loaded

--- main.ts
+++ main.ts
@@ -5,30 +5,35 @@ let output = document.getElementById("output"),
 let click = Observable.fromEvent(button, "click")
 
 function load(url: string) {
-	let xhr = new XMLHttpRequest();
+	return Observable.create(observer => {
+		let xhr = new XMLHttpRequest();
 
-	xhr.addEventListener("load", () => {
-		console.log('xhr.responseText', xhr.responseText)
-		let movies = JSON.parse(xhr.responseText);
-
-		movies.forEach((movie) => {
-			let div = document.createElement("div");
-			div.innerText = movie.title;
-			output.appendChild(div);
+		xhr.addEventListener("load", () => {
+			let data = JSON.parse(xhr.responseText);
+			observer.next(data);
+			observer.complete();
 		})
-	})
 
-	xhr.open("GET", url);
-	xhr.send();
+		xhr.open("GET", url);
+		xhr.send();
+	})
+}
+
+function renderMovies(movies) {
+	movies.forEach((movie) => {
+		let div = document.createElement("div");
+		div.innerText = movie.title;
+		output.appendChild(div);
+	})
 }
 
 
-click.subscribe(
-	e => load("movies.json"),
+click.flatMap(e => load("movies.json"))
+	.subscribe(renderMovies,
 	(e) => {
 		console.log(`error: ${e}`);
 	},
 	() => {
 		console.log('complete');
 	}
-);
+	);
